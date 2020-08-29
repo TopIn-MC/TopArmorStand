4d78ab475ca8e2a5860398fb32a364fd022b9da2
HSGamer <huynhqtienvtag@gmail.com>
2020-08-29 19:07:58 +0700

    do not damage top stand

--- src/main/java/me/hsgamer/topin/armorstand/ArmorStandListener.java
+++ src/main/java/me/hsgamer/topin/armorstand/ArmorStandListener.java
@@ -8,24 +8,34 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ArmorStandListener implements Listener {
 
   private final ConsumerRequestManager<ArmorStand> requestManager;
+  private final TopStandGetter getter;
 
   public ArmorStandListener(TopStandGetter getter) {
+    this.getter = getter;
     this.requestManager = getter.getRequestManager();
   }
 
   @EventHandler
-  public void onDamage(EntityDamageByEntityEvent event) {
+  public void onEntityDamage(EntityDamageByEntityEvent event) {
     Entity entity = event.getEntity();
     UUID uuid = event.getDamager().getUniqueId();
     if (entity instanceof ArmorStand && requestManager.contains(uuid)) {
       event.setCancelled(true);
       requestManager.apply(uuid, (ArmorStand) entity);
     }
+  }
+
+  @EventHandler
+  public void onBlockDamage(EntityDamageEvent event) {
+    Entity entity = event.getEntity();
+    event.setCancelled(
+        entity instanceof ArmorStand && getter.containsArmorStand(entity.getUniqueId()));
   }
 
   @EventHandler
