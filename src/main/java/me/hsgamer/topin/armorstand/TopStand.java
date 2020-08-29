43e8efc433a54cebf579a0041525848e50ea2c49
HSGamer <huynhqtienvtag@gmail.com>
2020-08-29 19:28:27 +0700

    equip silently

--- src/main/java/me/hsgamer/topin/armorstand/TopStand.java
+++ src/main/java/me/hsgamer/topin/armorstand/TopStand.java
@@ -39,6 +39,7 @@ public class TopStand implements ConfigurationSerializable {
     }
 
     ArmorStand armorStand = (ArmorStand) entity;
+    armorStand.setSilent(true);
     ItemStack itemStack = armorStand.getHelmet();
     if (itemStack == null) {
       return;
@@ -63,6 +64,7 @@ public class TopStand implements ConfigurationSerializable {
     OfflinePlayer topPlayer = Bukkit.getOfflinePlayer(dataList.getPair(index).getUniqueId());
     SkullUtils.setOwner((SkullMeta) itemMeta, topPlayer);
     itemStack.setItemMeta(itemMeta);
+    armorStand.setHelmet(itemStack);
   }
 
   public UUID getUniqueId() {
